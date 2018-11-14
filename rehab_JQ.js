13a5e77529a1605610a90e0e311572daf9b06f49
Rehab_Q <rkqarout1@sheffield.ac.uk>
2018-11-14 21:45:02 +0000

    Update rehab_JQ.js

--- rehab_JQ.js
+++ rehab_JQ.js
@@ -38,7 +38,7 @@ console.log("test");
 			console.log(not_eng_worker.value);
 			};
 		}
-	} return true); console.log("testdone");	   
+	}, true); console.log("testdone");	   
 
 
   $('#demo-form').parsley().on('form:submit', function(formInstance) {
